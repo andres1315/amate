0fc881c8ffb107db0d9fd9b42d2300c44a6fbb8e
Andres Duque <analista.desarrollo3@hyundailatinoamerica.com>
2022-11-04 16:52:59 -0500

    init repository

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header =()=>{
+  return(
+    <>
+      <header>
+        <h1>EyeLash by Tati</h1>
+        <button type="button">  Ingresar</button>
+      </header>
+    </>
+  )
+}
+
+
+export{
+  Header
+}
